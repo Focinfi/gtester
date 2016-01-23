666632a3e14d717ae229a43a4fe2bb13ee2b239f
Focinfi <focinfi@gmail.com>
2016-01-24 07:10:44 +0800

    httpmock: Add POSTForm

--- httpmock/httpmock.go
+++ httpmock/httpmock.go
@@ -1,8 +1,13 @@
 package httpmock
 
 import (
+	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
 )
 
 type httpMockClient struct {
@@ -22,6 +27,27 @@ func (client *httpMockClient) Dail(mehtod string, urlStr string, body io.Reader)
 	return response
 }
 
+func (client *httpMockClient) POSTForm(urlStr string, form map[string]interface{}) (*Recorder, error) {
+	params := url.Values{}
+	for key := range form {
+		switch value := form[key].(type) {
+		case string:
+			params.Set(key, value)
+		case int:
+			params.Set(key, strconv.Itoa(value))
+		default:
+			return nil, fmt.Errorf("has unexcepted type of value")
+		}
+	}
+
+	body := ioutil.NopCloser(strings.NewReader(params.Encode()))
+	request, _ := http.NewRequest("POST", urlStr, body)
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+	response := NewRecorder()
+	client.handler.ServeHTTP(response, request)
+	return response, nil
+}
+
 var defaultClient = &httpMockClient{}
 
 func ListenAndServe(host string, handler http.Handler) {
@@ -34,6 +60,10 @@ func GET(urlStr string, body io.Reader) *Recorder {
 
 func POST(urlStr string, body io.Reader) *Recorder {
 	return defaultClient.Dail("POST", urlStr, body)
+}
+
+func POSTForm(urlStr string, form map[string]interface{}) (*Recorder, error) {
+	return defaultClient.POSTForm(urlStr, form)
 }
 
 func PUT(urlStr string, body io.Reader) *Recorder {
