bb8f530f1c903154ce5dc6cc1f60f1752cc64f62
Focinfi <focinfi@gmail.com>
2016-02-29 18:00:57 +0800

    checkqueue.go Add Inspector

--- checkqueue.go
+++ checkqueue.go
@@ -27,3 +27,27 @@ func (cq *CheckQueue) Run() error {
 func NewCheckQueue() *CheckQueue {
 	return &CheckQueue{Checks: []func() error{}}
 }
+
+type Inspector struct {
+	checkQueue *CheckQueue
+}
+
+// NewInspector allocates and returns a new Inspector
+func NewInspector() *Inspector {
+	return &Inspector{NewCheckQueue()}
+}
+
+// Check receive checking to checkQueue
+func (i *Inspector) Check(checking func() error) *Inspector {
+	i.checkQueue.Add(checking)
+	return i
+}
+
+// Then execute the do if all checking passes
+func (i *Inspector) Then(do func()) error {
+	i.checkQueue.Run()
+	if i.checkQueue.Err == nil {
+		do()
+	}
+	return i.checkQueue.Err
+}
